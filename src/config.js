bdeb6ccecf35735b8197ea20e93c7bcfa800a8f7
R-Oscar <askar.payment@gmail.com>
2018-09-22 15:43:05 +0300

    added VK script

---
+++ src/config.js
@@ -0,0 +1,4 @@
+// export const token = '000b8840e91a13b4ffb8883a8c492f695e3d1c7f8c26550c1b8e8650d77cf628752fd8bff109614efa090';
+const apiId = 6673569;
+
+export default apiId;
--- src/index.jsx
+++ src/index.jsx
@@ -2,46 +2,59 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import './index.css';
+import apiId from './config';
 
 import CommunitiesSearchInput from './Components/CommunitiesSearchInput/CommunitiesSearchInput';
 import CommunitiesSearchResults from './Components/CommunitiesSearchResults/CommunitiesSearchResults';
 import CommunityInfo from './Components/CommunityInfo/CommunityInfo';
 
-const results = [
-  {
-    id: 23148107,
-    name: 'MDK',
-    photo: 'https://pp.vk.me/c624722/v624722728/48e8f/g2Z9jU6qXVk.jpg',
-  },
-  {
-    id: 23148108,
-    name: 'MDK',
-    photo: 'https://pp.userapi.com/c845520/v845520552/e5fb8/HR5Yhglt9TQ.jpg?ava=1',
-  },
-];
+import './index.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      results: [],
+      results: [
+        {
+          id: 23148107,
+          name: 'MDK',
+          photo: 'https://pp.vk.me/c624722/v624722728/48e8f/g2Z9jU6qXVk.jpg',
+        },
+        {
+          id: 23148108,
+          name: 'MDK',
+          photo: 'https://pp.userapi.com/c845520/v845520552/e5fb8/HR5Yhglt9TQ.jpg?ava=1',
+        },
+      ],
       input: '',
     };
   }
 
-  componentDidMount() {
-    // console.log(fetch);
-  }
-
-  handleInputChange = (e) => {
+  handleInputChange = async (e) => {
     this.setState({
       input: e.target.value,
     });
+
+    /* eslint-disable */
+    VK.init({
+      apiId,
+    });
+
+    VK.Api.call(
+      'groups.search', 
+      {
+        q: e.target.value,
+        count: 3,
+        offset: 3,
+        v: 5.85
+      }, 
+      r => console.log(r)
+    );
+    /* eslint-enable */
   };
 
   render() {
-    const { input } = this.state;
+    const { results, input } = this.state;
     return (
       <Router>
         <div className="wrapper">
